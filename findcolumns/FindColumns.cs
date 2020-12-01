fa60dee4e6069053310196d6cc09ed582a224270
KoteAnimen <sverh.il@yandex.ru>
2020-12-01 10:04:25 +0300

    Дома дописать прогу

--- findcolumns/FindColumns.cs
+++ findcolumns/FindColumns.cs
@@ -12,25 +12,7 @@ namespace findcolumns
     {
         public static int FindCountColumns(DataGridView table)
         {
-            int i, j = 0;
-            int countColumns = 0;
-            for(i = 0; i < table.Lengthl;sadvklpasd; i++)
-            {
-                int currentNumber = table[j][i];
-                for(j = 0; j < table.Length; j++)
-                {
-                    if(table[j][i] < currentNumber)
-                    {
-                        currentNumber--;
-                    }
-                    
-                }
-                if(currentNumber < table[j][i])
-                {
-                    countColumns++;
-                }
-            }
-            return countColumns;
+            
         }
     }
 }
